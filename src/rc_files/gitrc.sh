e8c7efce642e80d14e9e9d9b2594aacbfe3ea1c2
twoody <tanner.l.woody@gmail.com>
2021-10-30 22:42:49 -0700

    gdel update and simialr gch now

--- src/rc_files/gitrc.sh
+++ src/rc_files/gitrc.sh
@@ -13,7 +13,7 @@ alias gcm='git commit -message '
 alias gl='git log --stat' 
 alias gm='git merge' 
 alias gp='git pull' 
-alias gch='git checkout' 
+#alias gch='git checkout' 
 alias gitreset='git reset --hard head' 
 
 ### Get the changes done by a Git commit ##
@@ -25,9 +25,10 @@ gdc () {
 # Delete all branches matching the parameter
 # @param $1 - pattern to match
 gdel () {
-   #TODO: Write a help flag
-   #TODO: Write a force flag to skip over while loop and reads
-   #TODO: git checkout stage; If previous branch not deleted, recheckout previous branch;
+   WORKING_DIR=~/install-scripts
+   CONSTANTS_FILE=$WORKING_DIR/src/constants.sh
+   source $CONSTANTS_FILE
+   source $UTILS
    if [ $# -eq 0 ] ;
    then
       echo "No argument supplied"
@@ -39,33 +40,83 @@ gdel () {
    eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
    for branch in "${branches[@]}"; do
       mb=$(echo $branch | cut -d '/' -f 3)
+      mb2=$(echo $branch | cut -d '/' -f 4)
+
+      if [ ! -z $mb2 ]
+      then
+         mb=$mb/$mb2
+      fi
+
       if [[ $mb == 'stage' ]] ;
       then
          continue
       fi
       if [[ $mb == *$1* ]];
       then
-         while true; do
-         read -p "PERMANENTLY DELETE BRANCH $mb? (Y/N): " confirm
-            case $confirm in
-               [yY]*)
-                  printf "\tDELETING: $mb\n"
-                  git branch -D $mb
-                  break
-                  ;;
-            [nN]*)
-               printf "\tSKIPPING: $mb\n"
-               break
-               ;;
-            *)
-               printf "\tBAD INPUT\n"
-            esac
-         done
+         make_confirmation \
+           "PERMANENTLY DELETE BRANCH $mb?"\
+           "DELETING $mb\n"\
+           "SKIPPING $mb\n"
+         CONFIRMED=$?
+         if [ $CONFIRMED -eq 0 ];
+         then
+           git branch -D $mb
+           echo "\n"
+         fi
+      fi
+   done
+   return 0
+}
+
+# Checkout a branch but easy
+# @param $1 - pattern to match
+gch () {
+   WORKING_DIR=~/install-scripts
+   CONSTANTS_FILE=$WORKING_DIR/src/constants.sh
+   source $CONSTANTS_FILE
+   source $UTILS
+   #TODO: Write a force flag to skip over while loop and reads
+   if [ $# -eq 0 ] ;
+   then
+      echo "No argument supplied"
+      return 1
+   fi
+
+   branches=()
+   eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+
+   if [ ${#branches[@]} -eq 0 ] ;
+   then
+      printf "No branches matching '$1'\n"
+      return 1
+   fi
+
+   for branch in "${branches[@]}"; do
+      mb=$(echo $branch | cut -d '/' -f 3)
+      mb2=$(echo $branch | cut -d '/' -f 4)
+
+      if [ ! -z $mb2 ]
+      then
+         mb=$mb/$mb2
+      fi
+
+      if [[ $mb == *$1* ]];
+      then
+         make_confirmation \
+           "Switch to branch $mb?"\
+           "Checking out $mb\n"\
+           "SKIPPING $mb\n"
+         CONFIRMED=$?
+         if [ $CONFIRMED -eq 0 ];
+         then
+           git checkout $mb
+           break
+         fi
       fi
    done
    return 0
 }
 
 git-reset () {
-	git reset --hard head
+   git reset --hard head
 }
