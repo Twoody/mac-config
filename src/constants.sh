0672e905279c5be8eb49c8e941e57d1afa2cc183
Tanner Woody <tanner.l.woody@gmail.com>
2025-01-17 23:08:22 -0800

    remove references to arcanist

--- src/constants.sh
+++ src/constants.sh
@@ -9,7 +9,6 @@ SETUP=$INSTALL_DIR/workspace-setup.sh
 APPLICATIONS_INSTALL=$INSTALL_DIR/applications-install.sh
 PROGRAMS_INSTALL=$INSTALL_DIR/program-installs.sh
 VIM_INSTALL=$INSTALL_DIR/vim-installs.sh
-ARCANIST_INSTALL=$INSTALL_DIR/install-arcanist.sh
 
 DOCK_LIST=~/Library/Preferences/com.apple.dock.plist
 APP_SONIC_WALL=/Applications/SonicWall\ Mobile\ Connect.app
@@ -27,12 +26,8 @@ POWERLEVEL9K=$OH_MY_ZSH/custom/themes/powerlevel9k
 POWERLEVEL10K=$OH_MY_ZSH/custom/themes/powerlevel10k
 ZSH_SYNTAX_HIGHLIGHTER=~/.zsh-syntax-highlighting
 
-ad=/usr/local/bin/arcanist
-ld=/usr/local/bin/libphutil
-
 # TODO: Use these and set these up for proper installs
 VERSION_PHP=7.4
 VERSION_NODE=8.2
-BRANCH_ARCANIST=stable
 # https://github.com/antoniandre/vue-cal
 BRANCH_VUE_CAL=2.14.1
